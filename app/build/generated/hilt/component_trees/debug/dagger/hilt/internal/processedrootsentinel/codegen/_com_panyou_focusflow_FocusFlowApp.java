95dac41e6849126035722c43c7493bc5fac0a8ce
panyou <panyouxg@163.com>
2026-02-13 17:19:09 +0800

    fix: Correct icon namespace and include CI local.properties fix

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_panyou_focusflow_FocusFlowApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.panyou.focusflow.FocusFlowApp"
-)
-public final class _com_panyou_focusflow_FocusFlowApp {
-}
